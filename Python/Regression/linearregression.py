2cdba860623442de7f5c06c1899ffc2f85f8b9c9
amithmg6 <amithmg6@gmail.com>
2019-11-09 21:18:32 +0530

    Create linearregression.py

---
+++ Python/Regression/linearregression.py
@@ -0,0 +1,6 @@
+import pandas as pd
+from scipy import state
+import matplotlib.pyplot as myplot
+from pandas.tools.plotting import scatter_matrix
+from statsmodels.formula.api import ols
+from statsmodels.stats.anova import anova_lm

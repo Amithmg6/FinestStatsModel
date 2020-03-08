0ac28e975eec955410e89ad6994d5685efaf34c9
amithmg6 <amithmg6@gmail.com>
2020-03-08 10:01:05 +0530

    Create two_way_decision.py

---
+++ Python/beginner_course/two_way_decision.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 09:49:51 2020
+
+@author: Achuth MG
+"""
+
+""" two way decision """
+
+x = 4
+
+if x > 2:
+    print('bigger')
+else:
+    print('not bigger')
+print('all done')    

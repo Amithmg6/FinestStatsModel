42887725027ead880e43e6b5c913d7138a152f03
amithmg6 <amithmg6@gmail.com>
2020-03-08 16:15:36 +0530

    Create functions.py

---
+++ Python/beginner_course/functions.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 11:31:47 2020
+
+@author: Achuth MG
+"""
+
+""" Functions are reusable pieces of code 
+kinds of functions
+1. built-in-function
+2. defined functions"""
+
+def thing():
+    print('hello')
+    print('fun')
+    
+# call the function 
+
+thing() # defined function
+print('zip') # built in functions
+thing()

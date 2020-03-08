38a6d54790ea1fd48290853a5f255fde94f27557
amithmg6 <amithmg6@gmail.com>
2020-03-08 10:00:53 +0530

    Create multiway_conditions.py

---
+++ Python/beginner_course/multiway_conditions.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 09:52:36 2020
+
+@author: Achuth MG
+"""
+
+x = 500
+x = 0
+
+if x < 2:
+    print('small')
+elif x < 10:
+    print('medium')
+else:
+    print('large')
+print('all done')
+
+# NO ELSE 
+
+x = 5
+
+if x < 2:
+    print('small')
+elif x < 10:
+    print('medium')
+elif x < 20:
+    print('big')
+elif x < 40:
+    print('large')
+elif x < 100:
+    print('huge')
+else:
+    print('ginormous')

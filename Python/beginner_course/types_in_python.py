ce9564476c44ebad077b9018b5eaebdd97cfa168
amithmg6 <amithmg6@gmail.com>
2020-03-08 09:22:12 +0530

    Create types_in_python.py

---
+++ Python/beginner_course/types_in_python.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 14:05:02 2020
+
+@author: Achuth MG
+"""
+
+""" number is addition """
+a=1+4
+print(a)
+
+""" string is concatenation """
+
+e='hello'+'amith'
+print(e)
+
+""" Python knows the type of everything
+we can ask type by type() """
+
+type(e)
+
+type(a)
+
+""" numbers have two main types 
+1. integers are whole numbers : -14,-10,1,0,1000
+2. floating point numbers have a decimal part: -2.90, 0.0 1.2,1000.3904
+3. other numbers are variation of float and int """
+
+xx =1
+type(xx)
+
+temp = 99.12
+type(temp)
+
+""" type conversion  
+  INTEGER IS IMPLICITLY CONVERTED TO FLOAT 
+  control the above using the int() and float() functions """
+  
+print(float(99) + 1000)
+i=42
+f=float(i)
+print(f)
+type(f)
+
+""" int division produces a floating point result """
+ print(99/9)
+
+
+
+
+
+

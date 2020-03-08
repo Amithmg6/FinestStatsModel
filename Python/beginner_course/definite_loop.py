50e4318511f75b57548e85013e4a9d41ac9b3f21
amithmg6 <amithmg6@gmail.com>
2020-03-08 17:32:56 +0530

    Create definite_loop.py

---
+++ Python/beginner_course/definite_loop.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 17:04:01 2020
+
+@author: Achuth MG
+"""
+
+""" Definite loop """
+
+for i in [5,4,3,2,1] :
+    print(i)
+print('blastoff!')
+
+friends = ['john','arjun','siva','thomas']
+
+for friend in friends:
+    print('Happy New Year: ', friend)
+print('done')
+
+
+""" iteration variable
+    1.friend is an iteration variable here
+    2.iteration variable moves through all the variable in the sequence
+    3.iteration variable takes the value of the variable in the sequence
+"""
+
+    
+    
+    
+    

947cdc40538bd3036e3621900785e02e04f2d80a
amithmg6 <amithmg6@gmail.com>
2020-03-08 16:15:30 +0530

    Create arguments.py

---
+++ Python/beginner_course/arguments.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 15:51:55 2020
+
+@author: Achuth MG
+"""
+
+""" an argument is something we pass into the function as its input
+    we use arguments so we can direct the function to do 
+    different kinds of work when we call it at different times"""
+
+""" 'hello world' is an argument """
+
+big = max('hello world')
+print(big)
+
+
+
+

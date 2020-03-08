33c577d3b19aca6c2c8f2550644f14ee7ef14cd6
amithmg6 <amithmg6@gmail.com>
2020-03-08 11:10:38 +0530

    Create notry.py

---
+++ Python/beginner_course/notry.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 10:42:32 2020
+
+@author: Achuth MG
+"""
+
+astr = 'hello amith'
+istr = int(astr)
+print('First',istr)
+astr = '123'
+istr = int(astr)
+print('second', istr)
+
+# gets into error and stops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

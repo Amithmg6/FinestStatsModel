b2af788e5e32393abb1c7e57e363009ec440ecf8
amithmg6 <amithmg6@gmail.com>
2020-03-08 10:01:01 +0530

    Create one_way_decision.py

---
+++ Python/beginner_course/one_way_decision.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 09:39:57 2020
+
+@author: Achuth MG
+"""
+
+""" one way decision """
+
+x = 5
+print('Before 5')
+
+if x == 5: 
+    print('is 5')
+    print('is still 5')
+    print('Third 5')
+    
+print('Afterwards 5')
+
+print('Before 6')
+
+if x == 6:
+    print('is 6')
+    print('is still 6')
+    print('Third 6')
+
+print('Afterwards 6')
+
+

e769bd1d44786b85f70dd7c41e04270204322373
amithmg6 <amithmg6@gmail.com>
2020-03-08 09:21:36 +0530

    Create num_exp.py

---
+++ Python/beginner_course/num_exp.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 13:27:55 2020
+
+@author: Achuth MG
+"""
+
+xx = 2
+xx = xx + 2
+print('addition:',xx)
+
+yy = 440 * 12 
+print('multiplication:',yy)
+
+yy = 23
+zz = yy / 5
+print('quotient:',zz)
+
+jj=23
+kk=jj %5
+print('remainder:',kk)
+
+pp=8
+
+print(pp,'to power of 5','is =',pp**5)

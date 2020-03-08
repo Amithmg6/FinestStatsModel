f5726eb8184a8030eed058bebc0ded1f29376d6b
amithmg6 <amithmg6@gmail.com>
2020-03-08 09:39:08 +0530

    Update conditional.py

--- Python/beginner_course/conditional.py
+++ Python/beginner_course/conditional.py
@@ -8,9 +8,8 @@ Created on Sat Mar  7 13:12:50 2020
 """ conditional steps"""
 
 x = 5
-if x < 10:
-    print('smaller')
-if x> 20:
-    print('bigger')
+if x < 10: print('smaller')
+
+if x> 20: print('bigger')
      
 print('finish')

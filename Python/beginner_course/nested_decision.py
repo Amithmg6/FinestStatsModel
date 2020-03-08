31725415551a62985a2edf9320994d16346a7f8b
amithmg6 <amithmg6@gmail.com>
2020-03-08 10:00:57 +0530

    Create nested_decision.py

---
+++ Python/beginner_course/nested_decision.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 09:47:10 2020
+
+@author: Achuth MG
+"""
+
+""" nested decision """
+
+x = 42
+
+if x > 1: 
+    print('more than 1')
+    if x < 100:
+        print('less than 100')
+print('All done')

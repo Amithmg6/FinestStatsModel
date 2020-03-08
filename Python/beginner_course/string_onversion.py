f7b6b154aba54162bb3f3fe98f9920ca57ef8273
amithmg6 <amithmg6@gmail.com>
2020-03-08 09:22:07 +0530

    Create string_onversion.py

---
+++ Python/beginner_course/string_onversion.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar  7 14:16:49 2020
+
+@author: Achuth MG
+"""
+
+""" use the int() and float() to convert btw strings and integer """
+
+sval = '123'
+type(sval)
+
+print(sval + 1)
+
+ival = int(sval)
+type(ival)
+
+print(ival + 1)
+
+nsv = 'bob'
+niv = int(nsv)

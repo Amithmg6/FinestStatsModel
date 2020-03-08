85d3ee552ea6d97a5960769e9aa44baf0a5f76a7
amithmg6 <amithmg6@gmail.com>
2020-03-08 09:39:04 +0530

    Create comparision_operators.py

---
+++ Python/beginner_course/comparision_operators.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 09:24:34 2020
+
+@author: Achuth MG
+"""
+
+""" cmoparision operators in python 
+
+< is less than
+<= is less than or equal to
+== Equal to
+> greater than
+>= greater than or equal to
+!= not equal to
+
+remember "=" is used for assignment """
+
+# consider a value X
+
+x = 5
+
+if x == 5: print('equal to x')
+    
+if x < 6 : print('less than 6')
+
+if x <= 7: print('x is less than or equal to:', x)
+
+if x > 3: print('greater than 3')
+
+if x >= 5: print('greater than or equal to')
+
+if x != 6: print('x not equal')

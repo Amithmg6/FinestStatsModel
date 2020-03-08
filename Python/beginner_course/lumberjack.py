dd338a7231f7f042a69271736cab76cd12b47150
amithmg6 <amithmg6@gmail.com>
2020-03-08 16:15:41 +0530

    Create lumberjack.py

---
+++ Python/beginner_course/lumberjack.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  8 15:48:57 2020
+
+@author: Achuth MG
+"""
+
+x=5 
+print('hello')
+
+def print_lyrics():
+    print('i am me, i am okay.')
+    print('i sleep all night and i work all day')
+
+print('Yo amith')
+
+print_lyrics()
+
+x = x+ 2
+print(x)

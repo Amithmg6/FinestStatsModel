8a059fb5a66cb0752315354d4f18011611d8ea08
amithmg6 <amithmg6@gmail.com>
2020-03-25 22:25:01 +0530

    Update application.py

--- cs50w/lecture3/first/application.py
+++ cs50w/lecture3/first/application.py
@@ -1,7 +1,17 @@
 from flask import Flask
+"""
+app is a new application, a flask application.
+__name__ representing the current file
 
+"""
 app = Flask(__name__)
 
+"""
+flask is designed in terms of routes
+route is tpart of url
+/ default webpage
+when the user goes to / then this is the function (index) will run
+"""
 @app.route("/")
 def index():
     return "Hello, world!"
